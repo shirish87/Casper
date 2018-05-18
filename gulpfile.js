aa5179cc8d3051a3bbcecf18220607910a880635
Shirish Kamath <shirishk.87@gmail.com>
2018-05-18 11:58:57 +0530

    Added SCSS preprocessor support, gscan

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
+var sass = require('gulp-sass');
+var gscan = require("gscan");
+var chalk = require('chalk');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -13,6 +16,26 @@ var colorFunction = require('postcss-color-function');
 var cssnano = require('cssnano');
 var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
+
+var pkg = require('./package.json');
+
+var targetDir = 'dist/';
+var themeName = pkg.name;
+var filename = themeName + '.zip';
+var zipPath = targetDir + filename;
+
+var levels = {
+    error: chalk.red,
+    warning: chalk.yellow,
+    recommendation: chalk.yellow,
+    feature: chalk.green
+};
+
+var paths = {
+    scss: './assets/stylesheets/**/*.scss',
+    css: './assets/css/',
+    js: './assets/javascripts/**/*.js',
+};
 
 var swallowError = function swallowError(error) {
     gutil.log(error.toString());
@@ -28,7 +51,14 @@ gulp.task('build', ['css'], function (/* cb */) {
     return nodemonServerInit();
 });
 
-gulp.task('css', function () {
+gulp.task('sass', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass({ sourcemaps: true })
+    .on('error', sass.logError))
+    .pipe(gulp.dest(paths.css))
+});
+
+gulp.task('css', ['sass'], function () {
     var processors = [
         easyimport,
         customProperties,
@@ -37,7 +67,7 @@ gulp.task('css', function () {
         cssnano()
     ];
 
-    return gulp.src('assets/css/*.css')
+    return gulp.src(paths.css + '*.css')
         .on('error', swallowError)
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
@@ -47,14 +77,10 @@ gulp.task('css', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['css']);
+    gulp.watch(paths.css + '**', ['css']);
 });
 
 gulp.task('zip', ['css'], function () {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
-
     return gulp.src([
         '**',
         '!node_modules', '!node_modules/**',
@@ -66,4 +92,44 @@ gulp.task('zip', ['css'], function () {
 
 gulp.task('default', ['build'], function () {
     gulp.start('watch');
+});
+
+gulp.task('deploy', ['zip'], function () {
+    // based on: https://github.com/thoughtbot/ghost-theme-template/blob/e651bc70b65dae345e79e49122e59be7b93e7ad6/gulpfile.babel.js#L59
+
+    function outputResult(result) {
+        console.log('-', levels[result.level](result.level), result.rule);
+    }
+
+    function outputResults(theme) {
+        theme = gscan.format(theme);
+
+        console.log(chalk.bold.underline('\nRule Report:'));
+
+        if (theme.results.error && theme.results.error.length) {
+            console.log(chalk.red.bold.underline('\n! Must fix:'));
+            theme.results.error.forEach(outputResult);
+        }
+
+        if (theme.results.warning && theme.results.warning.length) {
+            console.log(chalk.yellow.bold.underline('\n! Should fix:'));
+            theme.results.warning.forEach(outputResult);
+        }
+
+        if (theme.results.recommendation && theme.results.recommendation.length) {
+            console.log(chalk.red.yellow.underline('\n? Consider fixing:'));
+            theme.results.recommendation.forEach(outputResult);
+        }
+
+        if (theme.results.pass && theme.results.pass.length) {
+            console.log(chalk.green.bold.underline('\n\u2713', theme.results.pass.length, 'Passed Rules'));
+        }
+
+        console.log('\n...checks complete.');
+    }
+
+    gscan.checkZip({
+        path: zipPath,
+        name: pkg.name
+    }).then(outputResults);
 });
